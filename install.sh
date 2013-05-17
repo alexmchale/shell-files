2438b3871d87dc1111f222b5a0152c3364bd15f5
Alex McHale <alex@anticlever.com>
2013-05-17 05:25:36 -0500

    Update installer to not do OSX-specific stuff on Linux.

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@
 MASTER_URL="https://nodeload.github.com/alexmchale/shell-files/tarball/master"
 MACVIM_TBZ="MacVim-snapshot-64"
 MACVIM_URL="http://cloud.github.com/downloads/b4winckler/macvim/$MACVIM_TBZ.tbz"
+OS="`uname -s`"
 
 
 
@@ -49,56 +50,125 @@ function announce {
 }
 
 
+if [ "$OS" = "Darwin" ]; then
 
-## Configure Xcode for local development ##
+  ## Configure Xcode for local development ##
 
-announce "Configuring Xcode"
-
-if [ -d /Applications/Xcode.app ]; then
-  echo "Xcode is installed!"
-  # sudo "/usr/bin/xcode-select" -switch "/Applications/Xcode.app/Contents/Developer"
-else
-  echo "You must have Xcode installed to proceed."
-  exit
-fi
-
-
-
-## Install or update Homebrew ##
-
-announce "Setting up Homebrew"
-
-if [ -x /usr/local/bin/brew ]; then
-  brew update
-else
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
-fi
+  announce "Configuring Xcode"
+  
+  if [ -d /Applications/Xcode.app ]; then
+    echo "Xcode is installed!"
+    # sudo "/usr/bin/xcode-select" -switch "/Applications/Xcode.app/Contents/Developer"
+  else
+    echo "You must have Xcode installed to proceed."
+    exit
+  fi
 
 
+  ## Install or update Homebrew ##
+  
+  announce "Setting up Homebrew"
+  
+  if [ -x /usr/local/bin/brew ]; then
+    brew update
+  else
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
+  fi
+  
+  
+  ## Install various Homebrew apps ##
+  
+  announce "Installing tools from Homebrew"
+  
+  brew_if_missing coreutils
+  brew_if_missing wget
+  brew_if_missing mysql
+  brew_if_missing postgresql
+  
+  
+  ## Install MacVim ##
+  
+  announce "Installing MacVim"
+  
+  if [ ! -d /Applications/MacVim.app ] || [ ! -x /usr/local/bin/mvim ]; then
+    cd /tmp
+    echo $MACVIM_URL
+    curl $MACVIM_URL | tar xj
+    cd $MACVIM_TBZ
+    rm -rf /Applications/MacVim.app
+    cp -a MacVim.app /Applications/
+    cp -a mvim /usr/local/bin/
+  fi
+  
+  
+  ## Configure Lion options ##
 
-## Install various Homebrew apps ##
-
-announce "Installing tools from Homebrew"
-
-brew_if_missing coreutils
-brew_if_missing wget
-brew_if_missing mysql
-brew_if_missing postgresql
-
-
-
-## Install MacVim ##
-
-announce "Installing MacVim"
-
-if [ ! -d /Applications/MacVim.app ] || [ ! -x /usr/local/bin/mvim ]; then
-  cd /tmp
-  echo $MACVIM_URL
-  curl $MACVIM_URL | tar xj
-  cd $MACVIM_TBZ
-  rm -rf /Applications/MacVim.app
-  cp -a MacVim.app /Applications/
-  cp -a mvim /usr/local/bin/
+  announce "Configuring OSX Lion"
+  
+  # Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
+  defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+  # Enable the 2D Dock
+  defaults write com.apple.dock no-glass -bool true
+  # Disable menu bar transparency
+  defaults write -g AppleEnableMenuBarTransparency -bool false
+  # Expand save panel by default
+  defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
+  # Expand print panel by default
+  defaults write -g PMPrintingExpandedStateForPrint -bool true
+  # Disable shadow in screenshots
+  defaults write com.apple.screencapture disable-shadow -bool true
+  # Enable highlight hover effect for the grid view of a stack (Dock)
+  defaults write com.apple.dock mouse-over-hilte-stack -bool true
+  # Enable spring loading for all Dock items
+  defaults write enable-spring-load-actions-on-all-items -bool true
+  # Disable press-and-hold for keys in favor of key repeat
+  defaults write -g ApplePressAndHoldEnabled -bool false
+  # Disable auto-correct
+  defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+  # Disable window animations
+  defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+  # Disable disk image verification
+  defaults write com.apple.frameworks.diskimages skip-verify -bool true
+  defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+  defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+  # Automatically open a new Finder window when a volume is mounted
+  defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+  defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+  # Avoid creating .DS_Store files on network volumes
+  defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+  # Disable Safari’s thumbnail cache for History and Top Sites
+  defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
+  # Enable Safari’s debug menu
+  defaults write com.apple.Safari IncludeDebugMenu -bool true
+  # Remove useless icons from Safari’s bookmarks bar
+  defaults write com.apple.Safari ProxiesInBookmarksBar "()"
+  # Disable send and reply animations in Mail.app
+  defaults write com.apple.Mail DisableReplyAnimations -bool true
+  defaults write com.apple.Mail DisableSendAnimations -bool true
+  # Disable Resume system-wide
+  defaults write com.apple.Safari NSQuitAlwaysKeepsWindows -bool false
+  defaults write com.apple.Terminal NSQuitAlwaysKeepsWindows -bool false
+  defaults write com.apple.QuickTimePlayerX NSQuitAlwaysKeepsWindows -bool false
+  defaults write com.google.Chrome NSQuitAlwaysKeepsWindows -bool false
+  defaults write com.apple.Terminal NSQuitAlwaysKeepsWindows -bool false
+  # Reset Launchpad
+  rm ~/Library/Application\ Support/Dock/*.db
+  # Show the ~/Library folder
+  chflags nohidden ~/Library
+  # Disable local Time Machine backups
+  sudo tmutil disablelocal
+  # Kill affected applications
+  for app in Safari Finder Dock Mail; do killall "$app" 2> /dev/null; done
+  # Fix for the ancient UTF-8 bug in QuickLook (http://mths.be/bbo)
+  echo "0x08000100:0" > ~/.CFUserTextEncoding
+  # Configure TextMate to open with Markdown by default.
+  defaults write com.macromates.textmate OakDefaultLanguage 0A1D9874-B448-11D9-BD50-000D93B6E43C
+  # Configure optimal TCP settings.
+  sudo sysctl -w net.inet.tcp.delayed_ack=0
+  sudo sysctl -w net.inet.tcp.slowstart_flightsize=10
+  # Disable iCloud being the default for save dialogs.
+  defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+  
 fi
 
 
@@ -121,117 +191,18 @@ for file in *; do
   cp -Ra $file ~/.$file
 done
 
+cd ~
 rm -rf /tmp/alexmchale-shell-files*
 
 
 
-## Install Vim modules ##
-
-announce "Installing Vim modules"
-
-mkdir -p ~/.vimbundles
-cd ~/.vimbundles
-clone_or_update rails git://github.com/tpope/vim-rails.git
-clone_or_update haml git://github.com/tpope/vim-haml.git
-clone_or_update nerdtree git://github.com/scrooloose/nerdtree.git
-clone_or_update vividchalk http://github.com/tpope/vim-vividchalk.git
-clone_or_update matchit http://github.com/tsaleh/vim-matchit.git
-clone_or_update fugitive http://github.com/tpope/vim-fugitive.git
-clone_or_update coffee-script http://github.com/kchmck/vim-coffee-script.git
-clone_or_update surround http://github.com/tpope/vim-surround.git
-clone_or_update gist http://github.com/mattn/gist-vim.git
-clone_or_update endwise http://github.com/tpope/vim-endwise.git
-clone_or_update align https://github.com/tsaleh/vim-align.git
-clone_or_update command-t git://git.wincent.com/command-t.git
-clone_or_update zencoding https://github.com/mattn/zencoding-vim.git
-
-
-
-## Command-T ##
-
-announce "Building Command-T for Vim"
-
-cd ~/.vimbundles/command-t/ruby/command-t
-/usr/bin/ruby extconf.rb
-make clean && make
-
-
-
-## Install rbenv ##
-
-announce "Installing rbenv"
-
-cd ~
-clone_or_update ".rbenv" "https://github.com/sstephenson/rbenv.git"
-clone_or_update ".ruby-build" "https://github.com/sstephenson/ruby-build.git"
-
-
-
-## Configure Lion options ##
-
-announce "Configuring OSX Lion"
-
-# Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
-defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
-# Enable the 2D Dock
-defaults write com.apple.dock no-glass -bool true
-# Disable menu bar transparency
-defaults write -g AppleEnableMenuBarTransparency -bool false
-# Expand save panel by default
-defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
-# Expand print panel by default
-defaults write -g PMPrintingExpandedStateForPrint -bool true
-# Disable shadow in screenshots
-defaults write com.apple.screencapture disable-shadow -bool true
-# Enable highlight hover effect for the grid view of a stack (Dock)
-defaults write com.apple.dock mouse-over-hilte-stack -bool true
-# Enable spring loading for all Dock items
-defaults write enable-spring-load-actions-on-all-items -bool true
-# Disable press-and-hold for keys in favor of key repeat
-defaults write -g ApplePressAndHoldEnabled -bool false
-# Disable auto-correct
-defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
-# Disable window animations
-defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
-# Disable disk image verification
-defaults write com.apple.frameworks.diskimages skip-verify -bool true
-defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
-defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
-# Automatically open a new Finder window when a volume is mounted
-defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
-defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
-# Avoid creating .DS_Store files on network volumes
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-# Disable Safari’s thumbnail cache for History and Top Sites
-defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
-# Enable Safari’s debug menu
-defaults write com.apple.Safari IncludeDebugMenu -bool true
-# Remove useless icons from Safari’s bookmarks bar
-defaults write com.apple.Safari ProxiesInBookmarksBar "()"
-# Disable send and reply animations in Mail.app
-defaults write com.apple.Mail DisableReplyAnimations -bool true
-defaults write com.apple.Mail DisableSendAnimations -bool true
-# Disable Resume system-wide
-defaults write com.apple.Safari NSQuitAlwaysKeepsWindows -bool false
-defaults write com.apple.Terminal NSQuitAlwaysKeepsWindows -bool false
-defaults write com.apple.QuickTimePlayerX NSQuitAlwaysKeepsWindows -bool false
-defaults write com.google.Chrome NSQuitAlwaysKeepsWindows -bool false
-defaults write com.apple.Terminal NSQuitAlwaysKeepsWindows -bool false
-# Reset Launchpad
-rm ~/Library/Application\ Support/Dock/*.db
-# Show the ~/Library folder
-chflags nohidden ~/Library
-# Disable local Time Machine backups
-sudo tmutil disablelocal
-# Kill affected applications
-for app in Safari Finder Dock Mail; do killall "$app" 2> /dev/null; done
-# Fix for the ancient UTF-8 bug in QuickLook (http://mths.be/bbo)
-echo "0x08000100:0" > ~/.CFUserTextEncoding
-# Configure TextMate to open with Markdown by default.
-defaults write com.macromates.textmate OakDefaultLanguage 0A1D9874-B448-11D9-BD50-000D93B6E43C
-# Configure optimal TCP settings.
-sudo sysctl -w net.inet.tcp.delayed_ack=0
-sudo sysctl -w net.inet.tcp.slowstart_flightsize=10
+# ## Install rbenv ##
+#
+# announce "Installing rbenv"
+#
+# cd ~
+# clone_or_update ".rbenv" "https://github.com/sstephenson/rbenv.git"
+# clone_or_update ".ruby-build" "https://github.com/sstephenson/ruby-build.git"
 
 
 
